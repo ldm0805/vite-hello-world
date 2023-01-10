50565a7ce7f9c7fd4f135f04b293c33dc26ba0f0
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-10 15:54:00 +0100

    Aggiunto export defaults in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
-<script setup>
+<script>
 // import HelloWorld from './components/HelloWorld.vue'
-import AppTitle from './components/AppTitle.vue'
-import AppHero from './components/AppHero.vue'
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+
+export default {
+  components: {
+    AppHero,
+    AppTitle,
+  },
+};
 </script>
+
 
 <template>
   <AppTitle />
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,6 +1,5 @@
 <script>
 export default {
-    name: "AppTitle",
     data(){
         return{
             hello: "La mia prima app con Vue"
